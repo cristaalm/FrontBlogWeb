13b5662fcbe4c9e97ccffc52f2256bdb724155d7
Gustavo <gceja2@ucol.mx>
2024-02-20 13:51:04 -0600

    React en login

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css'; // Asegúrate de que el path sea correcto
+
+// Componente para animar las etiquetas
+const AnimatedLabel = ({ text }) => (
+  <label>
+    {text.split('').map((letter, idx) => (
+      <span key={idx} style={{ transitionDelay: `${idx * 50}ms` }}>
+        {letter}
+      </span>
+    ))}
+  </label>
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section>
+      <div className="wave wave1"></div>
+      <div className="wave wave2"></div>
+      <div className="wave wave3"></div>
+      <div className="wave wave4"></div>
+      <div className="container">
+        <img src="src/img/logo without bg.png" alt="" />
+
+
+        <div class="inicio">
+            <h2 class="border">AquaVision</h2>
+            <h2 class="wa">AquaVision</h2>
+            </div>
+
+        <form>
+          <div className="form-control">
+            <input type="text" required />
+            <AnimatedLabel text="Correo" />
+          </div>
+          <div className="form-control">
+            <input type="password" required />
+            <AnimatedLabel text="Contraseña" />
+          </div>
+          <a href="#" className="login-link">
+            <span>inicio</span>
+            <div className="muv"></div>
+          </a>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          contador xd {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
