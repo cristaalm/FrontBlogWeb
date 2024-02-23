76754d75869274338068920d5c653c1a80d138fd
Adrian Lopez <aflores59@ucol.mx>
2024-02-23 05:15:57 -0600

    FixBugindx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css';
-import indexadmin from './indexadmin.jsx';
 import IndexAdmin from './indexadmin.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
