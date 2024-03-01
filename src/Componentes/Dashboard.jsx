69b105cc54f72953b9acbd972c5bc1ea7e673547
Gustavo <gceja2@ucol.mx>
2024-03-01 11:49:58 -0600

    tiny

--- src/Componentes/Dashboard.jsx
+++ src/Componentes/Dashboard.jsx
@@ -18,8 +18,9 @@ function Dashboard() {
     script.onload = () => {
       window.tinymce.init({
         selector: '#entryDescription',
-        // Opciones de configuración de TinyMCE
-        // ...
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    tinycomments_mode: 'embedded',
       });
     };
 
