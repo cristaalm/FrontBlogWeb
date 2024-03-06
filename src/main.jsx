3b13fc3420030f8cafe7c6cce610dd8999bbe4b2
Gustavo <gceja2@ucol.mx>
2024-03-06 07:24:23 -0600

    crud

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@
   import ReactDOM from 'react-dom/client'
   import App from './App.jsx'
   import Dashboard from './Componentes/Dashboard.jsx';
+import EntriesComponent from './Componentes/CRUD.jsx';
   //import './index.css';
 
   ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <App/>
+      <EntriesComponent/>
     </React.StrictMode>,
   )
