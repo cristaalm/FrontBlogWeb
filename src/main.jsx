c1145e8089fa1b38f51cd156346e7599827cfa40
miguel huitron <mhuitron0@ucol.mx>
2024-03-06 20:56:53 -0600

    mini arreglo

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import { createRoot } from 'react-dom/client';
 import CRUD from './Componentes/CRUD.jsx';
+import Dashboard from './jsx/components/Dashboard.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Dashboard />
   </React.StrictMode>
 );
 
