1e66a13a67c026c35ee75596c00a0c88e6d752d0
Adrian Lopez <aflores59@ucol.mx>
2024-03-08 08:38:03 -0600

    IndexMenuv3

--- src/home.jsx
+++ src/home.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import './home.css';
-import image1Url from './img/img1.png';
-import image2Url from './img/img2.png';
-import image3Url from './img/img3.png';
-import image4Url from './img/img4.png';
-import Carousel from 'react-bootstrap/Carousel';
-
+import Button from 'react-bootstrap/Button';
 
 function Home() {
   return (
@@ -23,14 +18,14 @@ function Home() {
           <button className='Quizz'>Quizz</button>
         </div>
         <div className='log'>
-            <button> usuario</button>
+          <Button variant="outline-success">Log in</Button>{' '}
         </div>
       </header>
 
       {/* Body */}
       <div className="bodyhome">
         <div className='Carrusel'>
-          <MyCarousel />
+          <video src="src/video/avwaves.mp4"autoPlay loop muted></video >
         </div> 
         <p>Aqui va ir el contenido para la web UwU.</p>
       </div>
@@ -46,44 +41,4 @@ function Home() {
   );
 }
 
-
-function MyCarousel() {
-  return (
-    <Carousel fade>
-      <Carousel.Item>
-        <ExampleCarouselImage src={image1Url} alt="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <ExampleCarouselImage src={image2Url} alt="Second slide" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <ExampleCarouselImage src={image3Url} alt="Third slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <ExampleCarouselImage src={image4Url} alt="Four slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
-}
-function ExampleCarouselImage({ src, alt }) {
-  return <img src={src} alt={alt} />;
-}
-
 export default Home;
-export { MyCarousel };
