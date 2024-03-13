9e5724325d8e66c78b0038998e3165c654d388ef
Adrian Lopez <aflores59@ucol.mx>
2024-03-13 13:36:11 -0600

    indexhomev4

--- src/home.jsx
+++ src/home.jsx
@@ -25,9 +25,54 @@ function Home() {
       {/* Body */}
       <div className="bodyhome">
         <div className='Carrusel'>
-          <video src="src/video/avwaves.mp4"autoPlay loop muted></video >
+          <video src="src/video/Aquavision.mp4"autoPlay loop muted></video >
         </div> 
-        <p>Aqui va ir el contenido para la web UwU.</p>
+        <div className= 'separador'>
+        </div>
+        <div className='contenedores'>
+          <div className='contenedor1'>
+            <div className='imagen1'>
+              <img src='/src/img/img1.png' alt='Imagen 1' />
+            </div>
+            <div className='titulo1'>
+              <h2>Título 1</h2>
+            </div>
+            <div className='contenido1'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+            </div>
+        </div>
+
+        <div className='contenedor2'>
+          <div className='imagen2'>
+            <img src='/src/img/img2.png' alt='Imagen 2' />
+          </div>
+          <div className='titulo2'>
+            <h2>Título 2</h2>
+          </div>
+          <div className='contenido2'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+
+        <div className='contenedor3'>
+          <div className='imagen3'>
+            <img src='/src/img/img3.png' alt='Imagen 3' />
+          </div>
+          <div className='titulo3'>
+            <h2>Título 3</h2>
+          </div>
+          <div className='contenido3' >
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+      <div className='quizz'>
+        <div className='body'>
+
+        </div>
+      </div>
+
+        </div>
       </div>
 
       {/* Footer */}
