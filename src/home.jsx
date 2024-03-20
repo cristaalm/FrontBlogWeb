e96cb4ed513472f64777700c04ea610c5531ea90
Adrian Lopez <aflores59@ucol.mx>
2024-03-19 22:31:20 -0600

    indexhomev5

--- src/home.jsx
+++ src/home.jsx
@@ -27,7 +27,10 @@ function Home() {
         <div className='Carrusel'>
           <video src="src/video/Aquavision.mp4"autoPlay loop muted></video >
         </div> 
-        <div className= 'separador'>
+        <div className= 'separador1'>
+          <div className='texto10'>
+            <h2>Top Categorias</h2>
+          </div>
         </div>
         <div className='contenedores'>
           <div className='contenedor1'>
@@ -35,7 +38,7 @@ function Home() {
               <img src='/src/img/img1.png' alt='Imagen 1' />
             </div>
             <div className='titulo1'>
-              <h2>Título 1</h2>
+              <h2>Lorem ipsum</h2>
             </div>
             <div className='contenido1'>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -48,7 +51,7 @@ function Home() {
             <img src='/src/img/img2.png' alt='Imagen 2' />
           </div>
           <div className='titulo2'>
-            <h2>Título 2</h2>
+            <h2>Lorem ipsum</h2>
           </div>
           <div className='contenido2'>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -60,18 +63,70 @@ function Home() {
             <img src='/src/img/img3.png' alt='Imagen 3' />
           </div>
           <div className='titulo3'>
-            <h2>Título 3</h2>
+            <h2>Lorem ipsum</h2>
           </div>
           <div className='contenido3' >
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
         </div>
-      <div className='quizz'>
         <div className='body'>
+        </div>
 
         </div>
       </div>
+      
+      {/* Unbody */}
+      <div className='separador2'>
+        <div className='texto11'>
+          <h2>Ultimas Entradas</h2>
+        </div>
+      </div>
+      <div className='contenedores2'>
+        <div className='contenedor11'>
+        <div className='imagen3'>
+            <img src='/src/img/img4.png' alt='Imagen 4' />
+          </div>
+          <div className='titulo3'>
+            <h2>Lorem ipsum</h2>
+          </div>
+          <div className='contenido3' >
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
 
+        </div>
+        <div className='contenedor12'>
+        <div className='imagen3'>
+            <img src='/src/img/img4.png' alt='Imagen 4' />
+          </div>
+          <div className='titulo3'>
+            <h2>Lorem ipsum</h2>
+          </div>
+          <div className='contenido3' >
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+        <div className='contenedor13'>
+        <div className='imagen3'>
+            <img src='/src/img/img4.png' alt='Imagen 4' />
+          </div>
+          <div className='titulo3'>
+            <h2>Lorem ipsum</h2>
+          </div>
+          <div className='contenido3' >
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+
+        </div>
+      </div>
+      <div className='separador3'>
+      </div>
+      <div className='quizzform'>
+        <div className='quizzimg'>
+          <img src='/src/img/logo.png'>
+        </img>
+        <div className='quizztext'>
+          <h2>Quizz</h2>
+        </div>
         </div>
       </div>
 
