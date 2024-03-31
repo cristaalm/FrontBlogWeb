303ebd1f8da06e7f10d0c8e4f509e97fd723f3c7
FREENKILLA <144183979+FREENKILLA@users.noreply.github.com>
2024-03-31 14:27:51 -0600

    fixvideo

--- src/home.jsx
+++ src/home.jsx
@@ -25,7 +25,7 @@ function Home() {
       {/* Body */}
       <div className="bodyhome">
         <div className='Carrusel'>
-          <video src="src/video/Aquavision.mp4"autoPlay loop muted></video >
+          <video src="src/video/aquavision10.mp4"autoPlay loop muted></video >
         </div> 
         <div className= 'separador1'>
           <div className='texto10'>
