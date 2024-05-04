7c07ffdcafa6f3c6cdf8c85ddccaa2165cb4bfef
miguel huitron <mhuitron0@ucol.mx>
2024-05-04 15:38:04 -0600

    mejora 404

--- src/jsx/Elements/Error404.jsx
+++ src/jsx/Elements/Error404.jsx
@@ -1,59 +1,81 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { CiCircleQuestion } from "react-icons/ci";
+import "../../css/animate404.css"
 
 const Error404 = ({ history }) => {
-  const goBack = () => {
-    history.goBack();
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh', // Abarca toda la altura visible del viewport
+    backgroundColor: '#95c2de',
   };
 
-  const styles = {
-    errorContainer: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh', // Esto asegura que el contenido esté centrado verticalmente en la página
-      backgroundImage: 'url("https://recla.org/wp-content/uploads/2023/10/mano-humana-tocando-agua-rio-crisis-agua.webp")', // Reemplaza "URL_DE_LA_IMAGEN" con la URL de la imagen de internet
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      padding: '20px',
-      color: '#fff', // Cambiamos el color del texto para que sea visible sobre el fondo
-    },
-    errorTitle: {
-      fontSize: '100px',
-      color: '#000',
-    },
-    errorMessage: {
-        fontSize: '38px',
-        color: '#FF5733', // Cambiamos el color del texto para que sea visible sobre el fondo
-        marginBottom: '20px',
-        
-    },
-    errorButton: {
-      backgroundColor: '#FF5733',
-      color: 'white',
-      border: 'none',
-      padding: '10px 20px',
-      fontSize: '16px',
-      cursor: 'pointer',
-      borderRadius: '5px',
-    },
-    errorButtonHover: {
-      backgroundColor: '#FF6347',
-    },
+  const mainboxStyle = {
+    backgroundColor: '#95c2de',
+    height: '600px',
+    width: '600px',
+    position: 'relative'
   };
+
+  const errStyle = {
+    color: '#ffffff',
+    fontFamily: 'Nunito Sans, sans-serif',
+    fontSize: '11rem',
+    position: 'absolute',
+    left: '20%',
+    top: '8%'
+  };
+
+  const errorIconStyle = {
+    position: 'absolute',
+    fontSize: '11rem',
+    left: '38%',
+    top: '15%',
+    color: '#ffffff'
+  };
+
+  const err2Style = {
+    color: '#ffffff',
+    fontFamily: 'Nunito Sans, sans-serif',
+    fontSize: '11rem',
+    position: 'absolute',
+    left: '68%',
+    top: '8%'
+  };
+
+  const msgStyle = {
+    textAlign: 'center',
+    fontFamily: 'Nunito Sans, sans-serif',
+    fontSize: '1.6rem',
+    position: 'absolute',
+    left: '16%',
+    top: '45%',
+    width: '75%'
+  };
+
+  
 
   return (
-    <div style={styles.errorContainer}>
-      <h1 style={styles.errorTitle}>404</h1>
-      <p style={styles.errorMessage}>Lo sentimos, la página que estás buscando no se ha encontrado.</p>
-      <Link to="/welcome">
-      <button style={styles.errorButton} onClick={goBack}>Volver</button>
-      </Link>
+<div style={containerStyle}>
+      <div style={mainboxStyle}>
+        <div className='conten_404'>
+          <div style={errStyle}>4</div>
+          {/* Aplica la clase fa-spin al icono */}
+          <CiCircleQuestion className="far fa-question-circle fa-spin" style={errorIconStyle}></CiCircleQuestion>
+          <div style={err2Style}>4</div>
+        </div>
+        <div style={msgStyle}>
+          Ooops..
+          <p>Page not found! Return to <Link to="/welcome"  className="home_link">home</Link>.</p>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Error404;
+
 
 
